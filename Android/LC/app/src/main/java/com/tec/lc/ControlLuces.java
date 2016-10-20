30fb738e613152726b1646a9f05517cfba79be1c
Laura <laurahidalgosoto@gmail.com>
2016-10-20 06:07:58 -0600

    pruebas

---
+++ Android/LC/app/src/main/java/com/tec/lc/ControlLuces.java
@@ -0,0 +1,13 @@
+package com.tec.lc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlLuces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_luces);
+    }
+}
