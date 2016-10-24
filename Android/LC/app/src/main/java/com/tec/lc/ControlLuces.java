93e1f95264bbb2e44bcbab780d59a1c5df01b18e
Laura <laurahidalgosoto@gmail.com>
2016-10-24 10:14:16 -0600

    dgs

--- Android/LC/app/src/main/java/com/tec/lc/ControlLuces.java
+++ Android/LC/app/src/main/java/com/tec/lc/ControlLuces.java
@@ -1,73 +1,33 @@
 package com.tec.lc;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
+import android.widget.Toast;
 
 public class ControlLuces extends AppCompatActivity {
-
-    private GoogleApiClient client;
-    private Button Luz3;
-    private Button Luz4;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button bL4;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_luces);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        bL4 = (Button) findViewById(R.id.Luz4);
+        bL4.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"bubububu4",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
-    public void makeUI(){
-    Luz3 = (Button)findViewById(R.id.Luz3);
-    }
+    public void L3OnClick(View v){
+        Toast.makeText(getBaseContext(),"bubububu3",Toast.LENGTH_LONG).show();
+        }//Close L3OnClick
 
-    public void L3OnClick(View v) {
-
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("ControlLuces Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
 }//End class
