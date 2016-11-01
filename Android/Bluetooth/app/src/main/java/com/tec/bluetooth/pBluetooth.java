307fd8537802ce441581710ea33265c31b241c6b
Laura <laurahidalgosoto@gmail.com>
2016-11-01 11:02:11 -0600

    pruebas

---
+++ Android/Bluetooth/app/src/main/java/com/tec/bluetooth/pBluetooth.java
@@ -0,0 +1,13 @@
+package com.tec.bluetooth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pBluetooth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_bluetooth);
+    }
+}
