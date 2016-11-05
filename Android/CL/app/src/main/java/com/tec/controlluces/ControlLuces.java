47a6c7764d08657c50b9275e7e7c87bc35690f29
Laura <laurahidalgosoto@gmail.com>
2016-11-05 10:51:15 -0600

    jj

--- Android/CL/app/src/main/java/com/tec/controlluces/ControlLuces.java
+++ Android/CL/app/src/main/java/com/tec/controlluces/ControlLuces.java
@@ -1,49 +1,47 @@
 package com.tec.controlluces;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
+import java.io.InputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 
 public class ControlLuces extends AppCompatActivity {
 
-    //Variables transmision de datos
-    private String serverIpAddress = "";//Buscar como capturarlo desde usuario
-    private Socket socket;
-    private int PORT = 5555;//revisar si hay q cambiar
-    private boolean addr = true;
+    //Creacion de variables
+    Boolean[] isPressed = {false};
+    Button bL3, bL4, bL5, bL6, bL7;
+    EditText changeAddress,changePort;
+    Button bConnect, bClear;
+    TextView tResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        //Definicion de variables
-        final Boolean[] isPressed = {false};
-        final Button bL3, bL4, bL5, bL6, bL7;
-
-        //Socket
-        try{
-            InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
-            socket = new Socket(serverAddr, PORT);
-        }//Close try
-        catch (UnknownHostException e) {
-            e.printStackTrace();}//close Catch UnknownHost
-        catch (IOException e) {
-            e.printStackTrace();}//Close IOException
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_luces);
 
-        final PrintWriter[] dout = new PrintWriter[1];//Variables necesarias
-        final String[] data = {"L"};//para transmision
+        //Definicion de variables
+        changeAddress = (EditText) findViewById(R.id.address);
+        changePort = (EditText) findViewById(R.id.port);
+        bConnect = (Button)findViewById(R.id.connect);
+        bClear = (Button)findViewById(R.id.clear);
+        tResponse = (TextView)findViewById(R.id.response);
+
+        bConnect.setOnClickListener(bConnectOnClickListener);
+        bClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tResponse.setText("");
+            } });
 
         //Boton Luz 3
         bL3 = (Button) findViewById(R.id.Luz3);
@@ -51,19 +49,13 @@ public class ControlLuces extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(isPressed[0]){ //led off
-                    bL3.setBackgroundResource(R.drawable.led_red);
-                    data[0] = "L3-0"; }//led off
+                    bL3.setBackgroundResource(R.drawable.led_red);}
                 else{ //led on
-                    bL3.setBackgroundResource(R.drawable.led_green);
-                    data[0] = "L3-1"; }//led on
+                    bL3.setBackgroundResource(R.drawable.led_green);}
                 isPressed[0] = !isPressed[0];
 
                 //Envio datos
-                try{
-                    dout[0] = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-                    dout[0].println(data[0]);
-                } catch (IOException e) {
-                    e.printStackTrace();}
+
             }
         });//Close L3.setOnClickListener
 
@@ -74,19 +66,12 @@ public class ControlLuces extends AppCompatActivity {
             public void onClick(View v) {
                 if(isPressed[0]){ //led off
                     bL4.setBackgroundResource(R.drawable.led_red);
-                    data[0] = "L4-0"; }
+                    }
                 else{
                     //led on
                     bL4.setBackgroundResource(R.drawable.led_green);
-                    data[0] = "L4-1"; }
+                    }
                 isPressed[0] = !isPressed[0];
-
-                //Envio datos
-                try{
-                    dout[0] = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-                    dout[0].println(data[0]);
-                } catch (IOException e) {
-                    e.printStackTrace(); }
             }
         });//Close L4.setOnClickListener
 
@@ -97,18 +82,11 @@ public class ControlLuces extends AppCompatActivity {
             public void onClick(View v) {
                 if(isPressed[0]){ //led off
                     bL5.setBackgroundResource(R.drawable.led_red);
-                    data[0] = "L5-0"; }
+                    }
                 else{//led on
                     bL5.setBackgroundResource(R.drawable.led_green);
-                    data[0] = "L5-1"; }
+                    }
                 isPressed[0] = !isPressed[0];
-
-                //Envio datos
-                try{
-                    dout[0] = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-                    dout[0].println(data[0]);
-                } catch (IOException e) {
-                    e.printStackTrace(); }
             }
         });//Close L5.setOnClickListener
 
@@ -117,21 +95,13 @@ public class ControlLuces extends AppCompatActivity {
         bL6.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if(isPressed[0]){
-                    //led off
+                if(isPressed[0]){//led off
                     bL6.setBackgroundResource(R.drawable.led_red);
-                    data[0] = "L6-0"; }
+                    }
                 else{//led on
                     bL6.setBackgroundResource(R.drawable.led_green);
-                    data[0] = "L6-1";}
+                    }
                 isPressed[0] = !isPressed[0];
-
-                //Envio datos
-                try{
-                    dout[0] = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-                    dout[0].println(data[0]);
-                } catch (IOException e) {
-                    e.printStackTrace();}
             }
         });//Close L6.setOnClickListener
 
@@ -140,32 +110,73 @@ public class ControlLuces extends AppCompatActivity {
         bL7.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if(isPressed[0]){
-                    //led off
+                if(isPressed[0]){//led off
                     bL7.setBackgroundResource(R.drawable.led_red);
-                    data[0] = "L7-0";}
-                else{
-                    //led on
+                    }
+                else{ //led on
                     bL7.setBackgroundResource(R.drawable.led_green);
-                    data[0] = "L7-1";}
+                    }
                 isPressed[0] = !isPressed[0];
-
-                //Envio datos
-                try{
-                    dout[0] = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-                    dout[0].println(data[0]);
-                } catch (IOException e) {
-                    e.printStackTrace();}
             }
         });//Close L7.setOnClickListener
 
-        //Envio datos
-        try{
-            dout[0] = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-            dout[0].println(data[0]);
-        } catch (IOException e) {
-            e.printStackTrace();}
-
     }//Close void onCreate
+
+    View.OnClickListener bConnectOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TaskClient myTaskClient = new TaskClient(changeAddress.getText().toString(), Integer.parseInt(changePort.getText().toString())) {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    Socket socket = null;
+
+                    try{
+                        socket = new Socket(destAdd,destPort);
+
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+                        InputStream inputStream = socket.getInputStream();
+
+                        while((bytesRead = inputStream.read(buffer))!= -1){
+                            byteArrayOutputStream.write(buffer, 0, bytesRead);
+                            resp += byteArrayOutputStream.toString("UTF-8");
+                        }//Close while
+                    }//Close try
+                    catch (UnknownHostException e) {
+                        e.printStackTrace();
+                        resp = "UnknownHostException: " + e.toString();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        resp = "IOException: " + e.toString();
+                    }finally {
+                        if(socket != null){
+                            try{
+                                socket.close();
+                            }//close try
+                            catch (IOException e) {
+                                e.printStackTrace(); }
+                        }//Close if socket
+                    }//Close finally
+                    return null;
+                }
+            };
+            myTaskClient.execute();
+        }};//Close bConnectOnClickListener
+
+    public abstract class TaskClient extends AsyncTask<Void, Void, Void>{
+        String destAdd;
+        int destPort;
+        String resp ="";
+        TaskClient(String addr, int port) {
+            destAdd = addr;
+            destPort = port;}//Close TaskClient
+
+
+        protected void postExecute(Void result){
+            tResponse.setText(resp);
+            super.onPostExecute(result);}//Close postExecute
+
+    }//Close TaskClient
 
 }//End Class
