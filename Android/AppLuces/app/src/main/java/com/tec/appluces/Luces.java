8adb88aa4e9aae2dc811b98bad34c062ef465fca
Laura <laurahidalgosoto@gmail.com>
2016-11-18 15:09:20 -0600

    work

--- Android/AppLuces/app/src/main/java/com/tec/appluces/Luces.java
+++ Android/AppLuces/app/src/main/java/com/tec/appluces/Luces.java
@@ -1,11 +1,8 @@
 package com.tec.appluces;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Luces extends AppCompatActivity {
@@ -16,23 +13,7 @@ public class Luces extends AppCompatActivity {
 
     private boolean addr = true;
 
-    class Hilo extends Thread{
-        //Se creo un hilo para trabajar con el socket y hacer envio de los datos
-        // (en este caso letras) para encender los leds
-        public void run(){
-            addr = true;
-            while (true){
-                try {
-                    BufferedReader data = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                } catch (IOException e) {
-                    //Se ejecuta en caso de que existan fallos
-                    //ademas se muestra el error ocurrido
-                    e.printStackTrace();
-                }
-            }//close while true
-        }//Close void run
-    }//Close class Hilo
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
