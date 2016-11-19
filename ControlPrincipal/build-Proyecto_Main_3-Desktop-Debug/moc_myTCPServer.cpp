fbd5751510629b8fd48b16faccbc12f40d3785ee
anthonychaves6 <tony9106@gmail.com>
2016-11-18 20:03:48 -0600

    Add files via upload

--- ControlPrincipal/build-Proyecto_Main_3-Desktop-Debug/moc_myTCPServer.cpp
+++ ControlPrincipal/build-Proyecto_Main_3-Desktop-Debug/moc_myTCPServer.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../Proyecto_Main_3/myTCPServer.h"
+#include "../Proyecto_3/myTCPServer.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
--- ControlPrincipal/build-Proyecto_Main_3-Desktop-Debug/moc_pantalla_principal.cpp
+++ ControlPrincipal/build-Proyecto_Main_3-Desktop-Debug/moc_pantalla_principal.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../Proyecto_Main_3/pantalla_principal.h"
+#include "../Proyecto_3/pantalla_principal.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
